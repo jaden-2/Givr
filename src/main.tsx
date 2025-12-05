73accf112450e3b64637d82f51e13604c69b4fc5
YudahTech <dammilare70@gmail.com>
2025-12-05 09:02:21 +0100

    I just removed the browseRouter module

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import LandingPage from './pages/landingpage'
-import { BrowserRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
       <LandingPage />
-    </BrowserRouter>
   </StrictMode>
 );
